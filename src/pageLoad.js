0441ae2ffad81547f3f165c527388235e0402ff6
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2023-08-29 23:12:01 -0400

    Added webpack config file

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -3,6 +3,8 @@ export default function pageLoad() {
 
     const pageHeader = document.createElement("h1");
     const description = document.createElement('p');
+    pageHeader.classList.add('title');
+    description.classList.add('desc');
     pageHeader.innerText = "Quick Bites";
     description.innerText = "At Quick Bites, we're redefining the fast food experience by bringing you mouthwatering flavors, lightning-fast service, and a dash of innovation with every bite. Our mission is simple: to deliver quick, delicious, and quality meals that keep up with your on-the-go lifestyle without compromising on taste or nutrition."
     contentDiv.appendChild(pageHeader);
